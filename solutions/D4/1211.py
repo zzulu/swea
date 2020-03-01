a369b310ccbd84c46b59b328ada8d0f4828908e9
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-03-01 23:11:56 +0900

    Fix 1211

--- solutions/D4/1211.py
+++ solutions/D4/1211.py
@@ -7,22 +7,21 @@ for _ in range(10):
 
     radder = [[int(n) for n in input().split()] for _ in range(100)]
 
-    startings = [i for i, v in enumerate(radder[0]) if v] # [0, 3, 12, 18, ...]
+    start_indices = [i for i, v in enumerate(radder[0]) if v] # [0, 3, 12, 18, ...]
 
     shortest, result = 0, 0
-    for index, starting in enumerate(startings): 
-        x, y, distance = startings[index], 0, 0
-        while y < 100:
+    for index, starting in enumerate(start_indices): 
+        x, distance = start_indices[index], 0
+        for y in range(100):
             if not x == 99 and radder[y][x+1]:
-                distance += startings[index+1] - startings[index]
+                distance += start_indices[index+1] - start_indices[index]
                 index += 1
 
             if not x == 0 and radder[y][x-1]:
-                distance += startings[index] - startings[index-1]
+                distance += start_indices[index] - start_indices[index-1]
                 index -= 1
 
-            x = startings[index]
-            y += 1
+            x = start_indices[index]
         
         if not shortest or shortest > distance:
             shortest = distance
